fb786e5b35ff882be985857822b7ba34da54cff2
rosamairani <rosarior@email.arizona.edu>
2017-02-01 22:36:49 -0700

    Add files via upload

--- NodeList.java
+++ NodeList.java
@@ -1,4 +1,4 @@
-package inforetrieval;
+//package inforetrieval;
 
 import java.util.ArrayList;
 import java.util.List;
--- problem5.java
+++ problem5.java
@@ -1,4 +1,4 @@
-package inforetrieval;
+//package inforetrieval;
 
 import java.io.File;
 import java.io.FileNotFoundException;
